15fd8e918041297612399b9037dfce56af3417e4
Opilane TTHK <opilane@tthk.ee>
2021-02-03 15:43:34 +0200

    Add project files.

---
+++ KinoAppDem/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KinoAppDem
+{
+    public partial class Form1 : Form
+    {
+        int i, j;
+        Label lbl;
+        Button btnS, btnM, btnL;
+        public Form1()
+        {
+            Height = 150;
+            Width = 360;
+            Text = "Выбор зала";
+
+            lbl = new Label();
+            lbl.Text = "Выберите зал в котором желаете совершить просмотр";
+            lbl.Size = new Size(300, 20);
+            lbl.Location = new Point(20, 10);
+            Controls.Add(lbl);
+
+            btnS = new Button();
+            btnS.Text = "Маленький зал";
+            btnS.Size = new Size(100, 30);
+            btnS.Location = new Point(20, 60);
+            btnS.Click += BtnS_Click;
+            Controls.Add(btnS);
+
+            btnM = new Button();
+            btnM.Text = "Средний зал";
+            btnM.Size = new Size(100, 30);
+            btnM.Location = new Point(120, 60);
+            btnM.Click += BtnM_Click;
+            Controls.Add(btnM);
+
+            btnL = new Button();
+            btnL.Text = "Большой зал";
+            btnL.Size = new Size(100, 30);
+            btnL.Location = new Point(220, 60);
+            btnL.Click += BtnL_Click;
+            Controls.Add(btnL);
+        }
+
+        private void BtnL_Click(object sender, EventArgs e)
+        {
+            i = 15; j = 15;
+            FormKino hall = new FormKino(i, j);
+            hall.Show();
+            Hide();
+        }
+
+        private void BtnM_Click(object sender, EventArgs e)
+        {
+            i = 10; j = 10;
+            FormKino hall = new FormKino(i, j);
+            hall.Show();
+            Hide();
+        }
+
+        private void BtnS_Click(object sender, EventArgs e)
+        {
+            i = 5; j = 5;
+            FormKino hall = new FormKino(i, j);
+            hall.Show();
+            Hide();
+        }
+    }
+}
---
+++ KinoAppDem/FormKino.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KinoAppDem
+{
+    public partial class FormKino : Form
+    {
+        int i, j;
+        Label[,] _arr;
+        public FormKino(int i_, int j_)
+        {
+            _arr = new Label[i_, j_];
+            Size = new Size(i_ * 80, j_ * 75);
+            Text = "Зал";
+            for (i = 0; i < i_; i++)
+            {
+                for (j = 0; j < j_; j++)
+                {
+                    _arr[i, j] = new Label();
+                    _arr[i, j].BackColor = Color.DarkGreen;
+                    _arr[i, j].Text = "Сиденье " + (j + 1);
+                    _arr[i, j].TextAlign = ContentAlignment.MiddleCenter;
+                    _arr[i, j].Size = new Size(55, 55);
+                    _arr[i, j].BorderStyle = BorderStyle.Fixed3D;
+                    _arr[i, j].Location = new Point(j * 55 + 55, i * 55);
+                    Controls.Add(_arr[i, j]);
+                    _arr[i, j].Tag = new int[] { i, j };
+                    _arr[i, j].Click += new System.EventHandler(FormKino_Click);
+                }
+            }
+        }
+
+        private void FormKino_Click(object sender, EventArgs e)
+        {
+            var label = (Label)sender;
+            var tag = (int[])label.Tag;
+            if(_arr[tag[0], tag[1]].Text != "Выбрано")
+            {
+                _arr[tag[0], tag[1]].Text = "Выбрано";
+                _arr[tag[0], tag[1]].BackColor = Color.DarkOrange;
+            }
+            else
+            {
+                MessageBox.Show("Ряд: " + (tag[0] + 1) + ", Сиденье: " + (tag[1] + 1) + " - уже занято!");
+            }
+        }
+    }
+}
