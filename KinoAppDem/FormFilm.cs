5200d3554c23734a15516ea9760b54c2a43936f9
opilane <opilane@HP-CZC6297B89>
2021-02-17 14:57:26 +0200

    Added images

--- KinoAppDem/FormFilm.cs
+++ KinoAppDem/FormFilm.cs
@@ -23,36 +23,41 @@ namespace KinoAppDem
             btn1 = new Button();
             btn1.Size = new Size(200, 200);
             btn1.Location = new Point(30, 40);
-            btn1.Image = Image.FromFile(@"C:\Users\opilane\source\repos\Demihhovski\KinoAppDem\KinoAppDem\Images\test.png");
+            btn1.BackgroundImage = Image.FromFile(@"C:\Users\opilane\source\repos\Demihhovski\KinoAppDem\KinoAppDem\Images\Terminator.png");
+            btn1.BackgroundImageLayout = ImageLayout.Stretch;
             btn1.Click += Btn1_Click;
             Controls.Add(btn1);
 
             btn2 = new Button();
             btn2.Size = new Size(200, 200);
             btn2.Location = new Point(290, 40);
+            btn2.BackgroundImage = Image.FromFile(@"C:\Users\opilane\source\repos\Demihhovski\KinoAppDem\KinoAppDem\Images\FastFurious.png");
+            btn2.BackgroundImageLayout = ImageLayout.Stretch;
             btn2.Click += Btn2_Click;
             Controls.Add(btn2);
 
             btn3 = new Button();
             btn3.Size = new Size(200, 200);
             btn3.Location = new Point(550, 40);
+            btn3.BackgroundImage = Image.FromFile(@"C:\Users\opilane\source\repos\Demihhovski\KinoAppDem\KinoAppDem\Images\Transformers.png");
+            btn3.BackgroundImageLayout = ImageLayout.Stretch;
             btn3.Click += Btn3_Click;
             Controls.Add(btn3);
 
             lbl1 = new Label();
-            lbl1.Text = "Film name 1";
+            lbl1.Text = "Terminator";
             lbl1.Size = new Size(70, 20);
             lbl1.Location = new Point(100, 260);
             Controls.Add(lbl1);
 
             lbl2 = new Label();
-            lbl2.Text = "Film name 2";
-            lbl2.Size = new Size(70, 20);
-            lbl2.Location = new Point(360, 260);
+            lbl2.Text = "Fast and Furious";
+            lbl2.Size = new Size(90, 20);
+            lbl2.Location = new Point(350, 260);
             Controls.Add(lbl2);
 
             lbl3 = new Label();
-            lbl3.Text = "Film name 3";
+            lbl3.Text = "Transformers";
             lbl3.Size = new Size(70, 20);
             lbl3.Location = new Point(620, 260);
             Controls.Add(lbl3);
